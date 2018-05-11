f601e48d3b00bbebac0cdaaa204a5a6422f0310e
danisafe3 <37106410+danisafe3@users.noreply.github.com>
2018-05-11 10:04:54 +0200

    Update sort.c

--- sort.c
+++ sort.c
@@ -5,4 +5,21 @@
 #include "sort.h"
 
 void sort(char** toSort, int size){ 
+    int i, j;
+    char buffer[size ];
+    
+    for(i = 0; i < size; i++){
+        j = i+1;
+        printf("\nIteracion %d\n", i);
+
+        while (j < size) {
+            if (strcmp(toSort[i], toSort[j]) > 0) {
+                printf("Swapping \"%s\" and \"%s\"\n", toSort[i], toSort[j]);
+                strncpy(buffer, toSort[i], size);
+                strncpy(toSort[i], toSort[j], size);
+                strncpy(toSort[j], buffer, size);
+            }
+            j++;
+        }
+    }
 }
