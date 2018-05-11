a35f9d04c909be0ebe6ac19615ef44bb354967ef
danisafe3 <37106410+danisafe3@users.noreply.github.com>
2018-05-11 09:28:38 +0200

    Update word_info.c

--- word_info.c
+++ word_info.c
@@ -4,12 +4,15 @@
 #include "word_info.h"
 
 void create_word_info(WordInfo* wi, char* word, char* definition, char pos) {
+    strncpy(wi->word, word, MAX_WORD_LENGTH);
+    strncpy(wi->definition, definition, MAX_DEFINITION_LENGTH);
+    wi->pos = pos;
 }
 
 char* get_word(WordInfo* wi) {
-    return NULL;
+    return wi->word;
 }
 
 void print_word_info(WordInfo wi) {
-    printf("-> %s (%c): %s\n", "WORD", 'P', "DEFINITION");
+    printf("-> %s (%c): %s\n", wi.word, wi.pos, wi.definition);
 }
