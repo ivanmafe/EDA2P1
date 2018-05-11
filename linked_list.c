4257a2b3f88cafbf0bbb4fd88ed30e30773b1777
Iván Martínez Fernández <37107183+ivanmafe@users.noreply.github.com>
2018-05-11 10:27:31 +0200

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,25 +1,125 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "commons.h"
 #include "linked_list.h"
 
-void init_list(LinkedList* l) {
+void init_list(LinkedList* list) {
+    
+    Node* first;
+    first = (Node*) calloc (1,sizeof(Node));
+    
+    list->start = first; 
+    
+} //OK
+
+void clear_list(LinkedList* list) {
+    
+    Node* pos; //we create a auxiliary node equal to the first of the list
+    pos = list->start;
+    
+    
+    while (pos->next != NULL){
+        pos = pos->next; // we go to the last position of the list
+    }
+    
+    while (pos->prev != NULL){
+        
+        strcpy(pos->data.definition,"\0"); //we delete the word info
+        pos->data.pos = '\0';
+        strcpy(pos->data.word,"\0");
+       
+        pos = pos->prev; //we now evaluate the previous element
+        pos->next->prev = NULL; 
+        free (pos->next); //we release the first node
+        
+        pos->next = NULL;  // we delete the connection with the element we wanted to delete
+        
+    }
+    
+    free(list->start);  //we release the space of the first node we created
+    
+} //OK
+
+/*
+ * This funtion must be summoned by the one that takes the word and definition
+ * and sorts it in the hash table
+ */
+void insert_into_list(LinkedList* list, WordInfo wi) {
+    
+    
+    
 }
 
-void clear_list(LinkedList* l) {
-}
-
-void insert_into_list(LinkedList* l, WordInfo wi) {
-}
-
-WordInfo* find_first_in_list(LinkedList* l, char* word) {
+WordInfo* find_first_in_list(LinkedList* list, char* word) {
+    
+    Node *node;
+    WordInfo *ans;
+    
+    node = list->start;
+    while (node->next != NULL){
+       
+    if (strcmp (node->data.word,word) == 0 ){
+        
+        strcmp(ans->definition,node->data.definition);
+        strcmp(ans->word,node->data.word);
+        ans->pos = node->data.pos;
+        
+        return (ans);
+    }
+    
+    else {
+        
+        node = node->next;
+        
+    }
+    
+    }
+    
     return NULL;
-}
+} //OK
 
-int delete_from_list(LinkedList* l, char* word) {
-    return ERROR;
+int delete_from_list(LinkedList* list, char* word) {
+    
+    Node *node;
+    node = list->start;
+    
+    while (node->next != NULL){
+       
+    if (strcmp (node->data.word,word) == 0 ){
+        
+        strcpy(node->data.definition,"\0");
+        strcpy(node->data.word,"\0");
+        node->data.pos = '\0';
+        
+        node->next->prev = node->prev;
+        node->prev->next = node->next;
+        node->next = NULL;
+        node->prev = NULL;
+        
+        free (node);
+        return (1);
+    }
+    else {
+        node = node->next;
+    }
+   
 }
+        return ERROR;
+    
+}//OK
 
-void print_list(LinkedList* l) {
-}
+void print_list(LinkedList* list) {
+    
+    Node* xd;
+    xd = list->start;
+    
+    printf("Words with : %c", xd->data.pos); 
+    
+    while (xd->next != NULL){
+        printf("\t%s  :  %s",xd->data.word,xd->data.definition);
+       
+    }
+    
+} //OK
