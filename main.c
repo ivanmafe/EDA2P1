85e6b7d203b0806dad6481b1294d980b6a985c80
Iván Martínez Fernández <37107183+ivanmafe@users.noreply.github.com>
2018-05-24 20:16:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -19,27 +19,28 @@ int main(int argc, char *argv[]) {
     init_table(&dict);
     
     int i;
+    
     for(i=0; i<info_size; i++) {
-        //print_word_info(info_list[i]);
-        int sol = exists_word(&dict, info_list[i]);
-        if(sol == FALSE){
-            int res = insert_word_info(&dict, info_list[i]);
-            if (res == ERROR){
-                printf("ERROR: Palabra \"%s\" no introducida correctamente en el diccionario.\n", info_list[i].word);
-            }else printf("Introducida la palabra \"%s\" en el diccionario.\n", info_list[i].word);
-        }else{
-            printf("La palabra \"%s\" ya está en el diccionario.\n", info_list[i].word);
-        }
+        
+        
+        if (exists_word(&dict,info_list[i].word) == FALSE){
+            insert_word_info(&dict, info_list[i]);
+            print_word_info(info_list[i]);}
+        else
+            printf("XX \"%s\" already exists \n",info_list[i].word);
     }
+    
+    
     printf("Dictionary loaded!\n");
 
-    printf("\nTask 4. Search from query list\n\n");
+    printf("\nTask 4. Search from query list\n\n");  
+    
     char* query_list[MAX_LINES];
     int query_size = read_query_file(QUERY_FILE_PATH, query_list);
-    
+  
     int j;
     for(j=0; j<query_size; j++) {
-        //printf("%s\n", query_list[i]);
+        //printf(" %s\n", query_list[j]);
         WordInfo* wi = find_word(&dict, query_list[j]);
         if (wi == NULL) {
             printf("XX %s (Not found!)\n", query_list[j]);
