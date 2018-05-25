00e8acb7b56354a43ef2cf474a8a542ced5d95b5
Iván Martínez Fernández <37107183+ivanmafe@users.noreply.github.com>
2018-05-25 17:57:51 +0200

    Update sort.c

--- sort.c
+++ sort.c
@@ -6,20 +6,20 @@
 
 void sort(char** toSort, int size) {
 
-    
+
     if (size > 1) {
-        
+
         int i = 0; //we start by the position 1 (makes no sense to start by 0)
         int j = 1; //previous element on the array
         char *aux;
-        
+
         while (j < size) {
             int a = i;
             int b = j;
-            while ( (strcmp(toSort[a], toSort[b]) > 0) && (b < size)) {
-                    aux = toSort[a];
-                    toSort[a] = toSort[b];
-                    toSort[b] = aux;
+            while ((strcmp(toSort[a], toSort[b]) > 0) && (b < size)) {
+                aux = toSort[a];
+                toSort[a] = toSort[b];
+                toSort[b] = aux;
 
                 a++;
                 b++;
@@ -27,11 +27,10 @@ void sort(char** toSort, int size) {
             i++;
             j++;
         }
-        
+
     }
 
 }
-
 
 
 
