5eeeae3b4f605835c2ab8cd877b7eaff9d0362f3
danisafe3 <37106410+danisafe3@users.noreply.github.com>
2018-05-27 13:49:10 +0200

    Update file_utils.h

--- file_utils.h
+++ file_utils.h
@@ -1,3 +1,15 @@
+/* 
+ * file_utils.h
+ *
+ * EDA II - Practice 1
+ * Practice Group : P302
+ *    - Daniel Sanchez (207554)
+ *    - Joan Moya (206659)
+ *    - Iván Martínez (206638)
+ * Group ID : 33
+ * 
+ */
+
 #ifndef UTIL_H
 #define UTIL_H
 
