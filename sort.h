2c546685fa9dfa7a01391f4142af1a62d48f1223
JoanMoya99 <38790800+JoanMoya99@users.noreply.github.com>
2018-05-27 13:44:30 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,6 +1,17 @@
 #ifndef SORT_H
 #define SORT_H
 
+/*
+ * EDA II - Practice 1
+ * Practice Group : P302
+ *    - Daniel Sanchez (207554)
+ *    - Joan Moya (206659)
+ *    - Iván Martínez (206638)
+ * Group ID : 33
+ * 
+ *
+ */
+
 void sort(char** toSort, int size);
 
 #endif
