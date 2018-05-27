cfd3cfb7d1bed2e34cba6b799ed33ae275675009
danisafe3 <37106410+danisafe3@users.noreply.github.com>
2018-05-27 13:48:19 +0200

    Update file_utils.c

--- file_utils.c
+++ file_utils.c
@@ -1,3 +1,15 @@
+/* 
+ * file_utils.c
+ *
+ * EDA II - Practice 1
+ * Practice Group : P302
+ *    - Daniel Sanchez (207554)
+ *    - Joan Moya (206659)
+ *    - Iván Martínez (206638)
+ * Group ID : 33
+ * 
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
