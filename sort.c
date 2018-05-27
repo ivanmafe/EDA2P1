8f0af91c2d52b1f8a90f6b4d6e851d1e117d348d
JoanMoya99 <38790800+JoanMoya99@users.noreply.github.com>
2018-05-27 13:44:09 +0200

    Update sort.c

--- sort.c
+++ sort.c
@@ -4,6 +4,16 @@
 
 #include "sort.h"
 
+/*
+ * EDA II - Practice 1
+ * Practice Group : P302
+ *    - Daniel Sanchez (207554)
+ *    - Joan Moya (206659)
+ *    - Iván Martínez (206638)
+ * Group ID : 33
+ * 
+ *
+ */
 
 /* 
 La funcion sort es la que se encargará de que en caso de que en una letra de nuestro diccionario
